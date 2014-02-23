2ff168a68faa8d7cfa5b0794e578129fb360204b
Sho Hashimoto <hashimoto@shokai.org>
2014-02-24 02:29:18 +0900

    fix indent

--- capture_face_detect.rb
+++ capture_face_detect.rb
@@ -10,7 +10,7 @@ loop do
   image = capture.query
   image = image.resize OpenCV::CvSize.new 640, 360
   detector.detect_objects(image).each do |rect|
-   puts "detect!! : #{rect.top_left}, #{rect.top_right}, #{rect.bottom_left}, #{rect.bottom_right}"
+    puts "detect!! : #{rect.top_left}, #{rect.top_right}, #{rect.bottom_left}, #{rect.bottom_right}"
     image.rectangle! rect.top_left, rect.bottom_right, :color => OpenCV::CvColor::Red
   end
   window.show image
